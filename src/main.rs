3677a8546e8e816aafb5e015a315578d63a5e935
level-one-learner <levelonelearner@outlook.com>
2024-01-08 17:54:06 +1100

    list buckets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::load_defaults;
+use aws_sdk_s3 as s3;
+use anyhow::{Result, Ok};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = load_defaults(aws_config::BehaviorVersion::v2023_11_09()).await;
+    let client = s3::Client::new(&config);
+    let binding = client.list_buckets().send().await?;
+    let buckets = binding.buckets();
+    println!("buckets: {:?}", buckets);
+    Ok(())
 }
